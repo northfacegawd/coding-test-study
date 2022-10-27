bfb15fc5391377f53e868444c5c795f796866ae5
northfacegawd <chuck5732590@gmail.com>
2022-10-27 22:56:46 +0900

    보이는 학생 답 수정

--- section-2/보이는_학생.js
+++ section-2/보이는_학생.js
@@ -3,7 +3,7 @@
  *
  * 선생님이 N(1<=N<=1000)명의 학생을 일렬로 세웠습니다.
  * 일렬로 서 있는 학생의 키가 앞에 서부터 순서대로 주어질 때,
- * 맨 앞에 서 있는 선생님이 볼 수 있는 학생의 수를 구하는 프로그 램을 작성하세요.
+ * 맨 앞에 서 있는 선생님이 볼 수 있는 학생의 수를 구하는 프로그램을 작성하세요.
  * (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
  *
  * 입력설명 - N명의 학생의 키가 앞에서부터 순서대로 주어진다.
@@ -18,9 +18,13 @@
  * @param {Array<number>} heights
  */
 function solution(heights) {
-  let answer = 0;
+  let answer = 0,
+    max = 0;
   for (let i = 0; i < heights.length; i++) {
-    if (heights[i] < heights[i + 1]) answer++;
+    if (heights[i] > max) {
+      answer++;
+      max = heights[i];
+    }
   }
   return answer;
 }
