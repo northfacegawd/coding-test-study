470462194a312cf272cb3a595152a9f1ec62020e
northfacegawd <chuck5732590@gmail.com>
2022-10-28 20:26:04 +0900

    가위바위보 답 추가

--- section-2/가위바위보.js
+++ section-2/가위바위보.js
@@ -19,23 +19,28 @@
 function solution(n, a, b) {
   let answer = [];
   for (let i = 0; i < n; i++) {
-    switch (a[i]) {
-      case 1:
-        if (b[i] === 1) answer.push("D");
-        if (b[i] === 2) answer.push("B");
-        if (b[i] === 3) answer.push("A");
-        break;
-      case 2:
-        if (b[i] === 1) answer.push("A");
-        if (b[i] === 2) answer.push("D");
-        if (b[i] === 3) answer.push("B");
-        break;
-      case 3:
-        if (b[i] === 1) answer.push("B");
-        if (b[i] === 2) answer.push("A");
-        if (b[i] === 3) answer.push("D");
-        break;
-    }
+    // switch (a[i]) {
+    //   case 1:
+    //     if (b[i] === 1) answer.push("D");
+    //     if (b[i] === 2) answer.push("B");
+    //     if (b[i] === 3) answer.push("A");
+    //     break;
+    //   case 2:
+    //     if (b[i] === 1) answer.push("A");
+    //     if (b[i] === 2) answer.push("D");
+    //     if (b[i] === 3) answer.push("B");
+    //     break;
+    //   case 3:
+    //     if (b[i] === 1) answer.push("B");
+    //     if (b[i] === 2) answer.push("A");
+    //     if (b[i] === 3) answer.push("D");
+    //     break;
+    // }
+    if (a[i] === b[i]) answer.push("D");
+    else if (a[i] === 1 && b[i] === 3) answer.push("A");
+    else if (a[i] === 2 && b[i] === 1) answer.push("A");
+    else if (a[i] === 3 && b[i] === 2) answer.push("A");
+    else answer.push("B");
   }
   return answer;
 }
