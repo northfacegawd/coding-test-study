c556fd3f08479dd29d11356f3b4046c068331831
northfacegawd <chuck5732590@gmail.com>
2022-10-28 22:22:12 +0900

    회문 문자열 답 추가

--- section-3/회문_문자열.js
+++ section-3/회문_문자열.js
@@ -17,10 +17,12 @@
  */
 function solution(str) {
   str = str.toLocaleLowerCase();
-  let len = str.length;
-  for (let i = 0; i < Math.floor(len / 2); i++) {
-    if (str[i] != str[len - i - 1]) return "NO";
-  }
+  // let len = str.length;
+  // for (let i = 0; i < Math.floor(len / 2); i++) {
+  //   if (str[i] != str[len - i - 1]) return "NO";
+  // }
+  if (str.split("").reverse().join("") !== str) return "NO";
+
   return "YES";
 }
 
