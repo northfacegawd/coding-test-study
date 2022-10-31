1d3ea50807151f65bda17104b6a60348a12ecaed
northfacegawd <chuck5732590@gmail.com>
2022-10-31 21:37:35 +0900

    쇠막대기

---
+++ section-6/쇠막대기.js
@@ -0,0 +1,20 @@
+/**
+ * @description 자료구조 - 스택
+ * @param {string} str
+ */
+function solution(str) {
+  let answer = 0;
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") stack.push(str[i]);
+    else {
+      stack.pop();
+      // 레이저
+      if (str[i - 1] === "(") answer += stack.length;
+      else answer += 1;
+    }
+  }
+  return answer;
+}
+
+console.log(solution("()(((()())(())()))(())"));
