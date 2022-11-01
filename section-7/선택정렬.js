3752658e564db25aefc034da36fb5bc6051a869e
northfacegawd <chuck5732590@gmail.com>
2022-11-01 22:43:57 +0900

    선택정렬

---
+++ section-7/선택정렬.js
@@ -0,0 +1,17 @@
+/**
+ * @description 정렬 - 선택정렬
+ * @param {Array<number>} arr
+ */
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) idx = j;
+    }
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+  }
+  return answer;
+}
+
+console.log(solution([13, 5, 11, 7, 23, 15]));
