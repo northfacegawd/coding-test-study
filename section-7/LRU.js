e757aa32bcc159c9777a6776c669d360586ae0ca
northfacegawd <chuck5732590@gmail.com>
2022-11-01 23:49:25 +0900

    LRU 답 추가

--- section-7/LRU.js
+++ section-7/LRU.js
@@ -5,6 +5,7 @@
  */
 function solution(size, arr) {
   let answer = Array.from({ length: size }, () => 0);
+  //   삽입정렬로
   arr.forEach((x) => {
     let pos = -1;
     for (let i = 0; i < size; i++) {
@@ -23,6 +24,24 @@ function solution(size, arr) {
     }
     answer[0] = x;
   });
+
+  // 내장함수 사용
+  //   let answer = [];
+  //   arr.forEach((x) => {
+  //     let pos = -1;
+  //     for (let i = 0; i < size; i++) {
+  //       if (x === answer[i]) {
+  //         pos = i;
+  //       }
+  //     }
+  //     if (pos === -1) {
+  //       answer.unshift(x);
+  //       if (answer.length > size) answer.pop();
+  //     } else {
+  //       answer.splice(pos, 1);
+  //       answer.unshift(x);
+  //     }
+  //   });
 
   return answer;
 }
